941843d1ae2eed72a4468f1846d987407f7df96b
gitHritik <97736648+gitHritik@users.noreply.github.com>
2025-09-24 16:49:11 +0530

    addc :

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
+    origin: ["https://ticket-less-frontend-ou6z.vercel.app/", "https://ticket-less-frontend-ou6z.vercel.app/"],
     methods: "GET, POST, PATCH, DELETE, PUT",
     credentials: true,
   })
